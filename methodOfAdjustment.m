4114df915737f8226af14305ab275df8df0b5e73
John Franchak <franchak@gmail.com>
2016-09-22 17:53:56 -0700

    fixed exit conditions in moa

--- methodOfAdjustment.m
+++ methodOfAdjustment.m
@@ -1,9 +1,9 @@
 function [responses_moa] = methodOfAdjustment(num_trials, start_dir)
 %Start_dir 1 equals narrow, start_dir 0 equals wide
+%Can exit prematurely, responses will be length of actual judgments
+%collected
 
-responses_moa = zeros(num_trials,1) - 1; %store responses
 trial_num = 1;
-
 while(1)
     %Alternate MOA direction
     if mod(trial_num,2) == start_dir
@@ -14,14 +14,15 @@ while(1)
     reply = input(sprintf('JUDGMENT #%d %s>> ',trial_num,direction),'s');    
     %If reply is a number, record value and go to next trial
     if ~isnan(str2double(reply))
-        responses_moa(trial_num) = str2double(reply);
+        responses_moa(trial_num) = str2double(reply); %#ok<*AGROW>
         trial_num = trial_num + 1;
     elseif strcmp(reply, 'exit')
         break;
     else
         disp('Input not recognized, please enter a number')
     end  
-    if trial_num == num_trials
+    %Break when we've passed the alloted number of trials
+    if trial_num == num_trials + 1
         break;
     end    
 end
